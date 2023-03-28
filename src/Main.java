51348efbe1056b8fdfee9ad1aaad116f95d45f89
ritakool <jokernguyen1994@gmail.com>
2023-03-28 17:13:20 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.setName("a");
+        st.setNameclass("C033");
+        System.out.println(st.name);
+        System.out.println(st.nameclass);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    protected String name = "John";
+    protected String nameclass = "CO2";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Student() {
+    }
+    public void setNameclass(String nameclass) {
+        this.nameclass = nameclass;
+    }
+}
